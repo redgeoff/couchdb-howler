82f6c744af9c51c8ec377091349d022cd60dcbf9
Geoff Cox <redgeoff@gmail.com>
2017-12-19 09:45:25 -0800

    fix(docker-entrypoint): hyphens not allowed in env vars

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,9 +2,34 @@
 
 set -e
 
+# All command line params in alpha order without the starting --
+declare -a params=(
+  "couchdb_url"
+  "help"
+  "log_level"
+  "port"
+  "version"
+)
+
 if [ "$1" = '/usr/local/bin/couchdb-howler' ]; then
 
-  $@
+  cmdParams=''
+
+  for i in "${params[@]}"
+  do
+
+    dockerParam=${i}
+
+    # Append to the list of params and values
+    if [ "${!dockerParam}" != "" ]; then
+      cmdParams="$cmdParams --$i=${!dockerParam}"
+    fi
+
+  done
+
+  echo "cmdParams="$cmdParams
+
+  $@ $cmdParams
 
 else
 
