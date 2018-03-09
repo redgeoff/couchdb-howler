a75b9be420e7b122e667e1f13fc59674c0f09d9d
Geoff Cox <redgeoff@gmail.com>
2018-03-09 12:45:18 -0800

    test(config): localhost causes issues with chrome

--- test/config.js
+++ test/config.js
@@ -1,24 +1,24 @@
 module.exports = {
   couchdb: {
     scheme: 'http',
-    host: 'localhost',
+    host: '127.0.0.1',
     port: 5984,
     username: 'admin',
     password: 'admin'
   },
   server1: {
     scheme: 'http',
-    host: 'localhost',
+    host: '127.0.0.1',
     port: 3000
   },
   server2: {
     scheme: 'http',
-    host: 'localhost',
+    host: '127.0.0.1',
     port: 3001
   },
   server3: {
     scheme: 'http',
-    host: 'localhost',
+    host: '127.0.0.1',
     port: 3002
   }
 }
